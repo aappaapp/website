104f819202637df8787eb9850b3feb531a16ffe3
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-09-23 23:52:37 +0800

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-function wiki_page(){
-    window.top.location.href = 'wiki.html'
-}
 function home_page(){
     window.top.location.href = "index.html"
+}
+function wiki_page(){
+    window.top.location.href = 'wiki.html'
 }
