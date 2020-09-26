863f7efd1a5cb65eb35e76005300d280042bc86c
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-09-26 21:08:33 +0800

    1

---
+++ assets/js/qrcode.js
@@ -0,0 +1,26 @@
+function onScanSuccess(qrMessage) {
+	// handle the scanned code as you like
+	console.log(`QR matched = ${qrMessage}`);
+}
+
+function onScanFailure(error) {
+	// handle scan failure, usually better to ignore and keep scanning
+	console.warn(`QR error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+	"reader", { fps: 10, qrbox: 250 }, /* verbose= */ true);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+// This method will trigger user permissions
+Html5Qrcode.getCameras().then(devices => {
+    /**
+     * devices would be an array of objects of type:
+     * { id: "id", label: "label" }
+     */
+    if (devices && devices.length) {
+      var cameraId = devices[0].id;
+      // .. use this to start scanning.
+    }
+  }).catch(err => {
+    // handle err
+  });
