ed1301354aad001d6c0b9a39168f85c875646855
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-10-04 00:22:41 +0800

    1

--- assets/js/addon.js
+++ assets/js/addon.js
@@ -1,6 +1,12 @@
 function download(downloadnum){
+    document.getElementById('config').style.display = "block";
+    document.getElementById('config-setting').innerHTML = 'What addon you choose, I can\'t know what you choose. <br>Please try to choose again';
     if(downloadnum == 1){
-        alert('Nothing Now');
-        
+        document.getElementById('config-setting').innerHTML = 'Nothing Now';
     }
 }
+window.addEventListener("click", function(event){
+    if(event.target == document.getElementById('config')){
+        document.getElementById('config').style.display = "none";
+    }
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 function home_page(){
-    window.top.location.href = 'index.html'
+    window.top.location.href = 'index.html';
 }
 function faq_page(){
-    window.top.location.href = 'faq.html'
+    window.top.location.href = 'faq.html';
 }
 function addon_page(){
-    window.top.location.href = 'mcaddon.html'
+    window.top.location.href = 'mcaddon.html';
+}
+function page_choose(){
+    window.top.location.href = 'page_choose.html'
 }
 function github_page(){
-    window.top.location.href = 'https://github.com/adenpun/adenpun.github.io'
+    window.top.location.href = 'https://github.com/adenpun/adenpun.github.io';
 }
