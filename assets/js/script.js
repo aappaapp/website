79df5db3484808e1d0d5078ba74678e74ce7700b
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-10-03 23:29:24 +0800

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ function faq_page(){
     window.top.location.href = 'faq.html'
 }
 function addon_page(){
-    window.top.location.href = 'addon.html'
+    window.top.location.href = 'mcaddon.html'
 }
 function github_page(){
     window.top.location.href = 'https://github.com/adenpun/adenpun.github.io'
