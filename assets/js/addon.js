7b33fed1eaac76c5200bdf79430221598da69692
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-10-04 10:08:08 +0800

    1

--- assets/js/addon.js
+++ assets/js/addon.js
@@ -1,6 +1,4 @@
 function download(downloadnum){
-    document.getElementById('config').classList.remove('jump-out-animation');
-    document.getElementById('config').classList.add('jump-animation');
     document.getElementById('config').style.display = 'block';
     document.getElementById('config-setting').innerHTML = 'What addon you choose, I can\'t know what you choose. <br>Please try to choose again';
     if(downloadnum == 1){
@@ -8,12 +6,10 @@ function download(downloadnum){
     }
 }
 window.addEventListener("click", function(event){
-    function displaynone(){
-        document.getElementById('config').style.display = 'none';
-    }
     if(event.target == document.getElementById('config')){
         document.getElementById('config').style.display = 'none';
-        document.getElementById('config').classList.remove('jump-animation');
-        document.getElementById('config').classList.add('jump-out-animation');
     }
 });
+function closeConfig(){
+    document.getElementById('config').style.display = 'none';
+}
