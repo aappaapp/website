9017bc4fcfcffde308d5eb05930ea261e11bf64b
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-10-04 10:22:02 +0800

    1

--- assets/js/addon.js
+++ assets/js/addon.js
@@ -2,9 +2,10 @@ function download(downloadnum){
     document.getElementById('config').style.display = 'block';
     document.getElementById('config-setting').innerHTML = 'What addon you choose, I can\'t know what you choose. <br>Please try to choose again';
     if(downloadnum == 1){
-        document.getElementById('config-setting').innerHTML = 'Nothing Now';
+        document.getElementById('config-setting').innerHTML = '<input type="text" placeholder="nothing now">';
     }
 }
+//to copy downloadnum = 1 ==> <input type="text" placeholder="nothing now">
 window.addEventListener("click", function(event){
     if(event.target == document.getElementById('config')){
         document.getElementById('config').style.display = 'none';
