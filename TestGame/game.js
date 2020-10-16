a9e71e7f75da5d802751427e7a2d07f12f4d2877
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-10-16 23:38:04 +0800

    1

--- TestGame/game.js
+++ TestGame/game.js
@@ -1,102 +1,172 @@
-var vh = $(window).height()
-var vw = $(window).width()
+var vh = $(window).height();
+var vw = $(window).width();
 $.fn.toUI = function(type){
     if(type == 'startbtn'){
-        this.addClass('starbtn')
+        this.addClass('starbtn');
     } else if(type == 'gamearea'){
-        this.addClass('gamarea')
+        this.addClass('gamarea');
     } else if(type == 'homepage'){
-        this.addClass('hompage')
+        this.addClass('hompage');
     } else if(type == 'pausebtn'){
-        this.addClass('pausbtn')
+        this.addClass('pausbtn');
     } else if(type == 'pausepage'){
-        this.addClass('pauspage')
+        this.addClass('pauspage');
     } else if(type == 'continuebtn'){
-        this.addClass('continubtn')
+        this.addClass('continubtn');
+    } else if(type == 'savebtn'){
+        this.addClass('savbtn');
     } else {
-        console.error('...toUI(\'' + type + '\') is wrong')
+        console.error('...toUI(\'' + type + '\') is wrong');
     }
 };
 $.fn.toSprite = function(type, config){
     if(type == 'sprite'){
-        this.addClass("sprte")
-        var thcstopstr = Number(this.css('top').substr(0, this.css('top').length - 2))
+        this.addClass('sprte');
+        var thcstopstr = Number(this.css('top').substr(0, this.css('top').length - 2));
         if(thcstopstr <= 400){
-            this.teleport('gravity', 0, 0)
+            this.teleport('gravity', 0, 0);
         }
-    } else if(type == 'control') {
-        this.addClass("sprte")
-        this.addClass("control")
+    } else if(type == 'control'){
+        this.addClass('sprte');
+        this.addClass('control');
         window.xspeed = config.split(',')[0];
         window.yspeed = config.split(',')[1];
-    } else if(type == 'enemy') {
-        this.addClass("sprte")
-        this.addClass("enemy")
+    } else if(type == 'enemy'){
+        this.addClass('sprte');
+        this.addClass('enemy');
+        this.addClass(config);
     } else {
-        console.error('...rite(\'' + type + '\') is wrong')
+        console.error('...rite(\'' + type + '\') is wrong');
     }
 };
 $.fn.teleport = function(type, x, y){
     if(this.hasClass('sprte') == true){
         if(type == ''){
-            this.css('left', x/10 + 'vw')
-            this.css('top', y/10 + 'vh')
+            this.css('left', x/10 + 'vw');
+            this.css('top', y/10 + 'vh');
         } else if(type == 'move'){
-            var thcstopstr = Number(this.css('top').substr(0, this.css('top').length - 2))
-            var thcsleftstr = Number(this.css('left').substr(0, this.css('left').length - 2))
-            this.css('left', thcsleftstr + x + 'px')
-            this.css('top', thcstopstr + y + 'px')
+            var thcstopstr = Number(this.css('top').substr(0, this.css('top').length - 2));
+            var thcsleftstr = Number(this.css('left').substr(0, this.css('left').length - 2));
+            this.css('left', thcsleftstr + x + 'px');
+            this.css('top', thcstopstr + y + 'px');
         } else {
-            console.error('...port(\'' + type + '\'...is wrong')
+            console.error('...port(\'' + type + '\'...is wrong');
         }
     } else {
-        console.error('id: ' + this.attr('id') + ' is not a sprite')
+        console.error('id: ' + this.attr('id') + ' is not a sprite');
     }
 };
+$.fn.overlap = function(firstitem, seconditem){
+	var d1_offset             = firstitem.offset();
+	var d1_height             = firstitem.outerHeight( true );
+	var d1_width              = firstitem.outerWidth( true );
+	var d1_distance_from_top  = d1_offset.top + d1_height;
+    var d1_distance_from_left = d1_offset.left + d1_width;
+    
+	// Div 2 data
+	var d2_offset             = seconditem.offset();
+	var d2_height             = seconditem.outerHeight( true );
+	var d2_width              = seconditem.outerWidth( true );
+	var d2_distance_from_top  = d2_offset.top + d2_height;
+	var d2_distance_from_left = d2_offset.left + d2_width;
+    window.overlap = seconditem.attr('class');
+
+	var not_colliding = ( d1_distance_from_top < d2_offset.top || d1_offset.top > d2_distance_from_top || d1_distance_from_left < d2_offset.left || d1_offset.left > d2_distance_from_left );
+
+	// Return whether it IS colliding
+	return ! not_colliding;
+};
+$.fn.intoFight = function(enemyname){
+    $('.' + enemyname).css('display', 'none')
+}
+$.fn.generate = function(){
+
+}
+$.fn.setCookie = function(cname, cvalue, exdays){
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+$.fn.getCookie = function(cname){
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+$.fn.checkCookie = function(){
+    console.log('d')
+    var coordinate = $().getCookie("coordinate");
+    if (coordinate != ""){
+        console.log('coordinate is ' + coordinate)
+    }
+}
 $(document).ready(function(){
+    $().checkCookie();
     $(window).contextmenu(function(event){
-        event.preventDefault()
-    })
+        event.preventDefault();
+    });
     $('.starbtn').click(function(){
-        $('.hompage').css('display', 'none')
-        $('.gamarea').css('display', 'block')
-        $('.pausbtn').css('display', 'block')
-    })
+        window.start = true;
+        $('.hompage').css('display', 'none');
+        $('.gamarea').css('display', 'block');
+        $('.pausbtn').css('display', 'block');
+    });
     $('.pausbtn').click(function(){
-        $('.pauspage').css('display', 'block')
-        $('.pausbtn').css('display', 'none')
-    })
+        $('.pauspage').css('display', 'block');
+        $('.pausbtn').css('display', 'none');
+    });
     $('.pauspage').click(function(event){
         if($(event.target).hasClass('pauspage')){
-            $('.pauspage').css('display', 'none')
-            $('.pausbtn').css('display', 'block')
+            $('.pauspage').css('display', 'none');
+            $('.pausbtn').css('display', 'block');
         }
-    })
+    });
     $('.continubtn').click(function(){
-        $('.pauspage').css('display', 'none')
-        $('.pausbtn').css('display', 'block')
-    })
+        $('.pauspage').css('display', 'none');
+        $('.pausbtn').css('display', 'block');
+    });
+    $('.savbtn').click(function(){
+        $().setCookie('coordinate', $('.control').css('top'), 365);
+    });
     $(window).keydown(function(){
-        var xspeed = Number(window.xspeed)
-        var yspeed = Number(window.yspeed)
-        if(event.which == 39){
-            $('.control').teleport('move', xspeed, 0)
-            $('.control').removeClass('flip')
-        } else if(event.which == 37){
-            $('.control').teleport('move', Number('-' + xspeed), 0)
-            $('.control').addClass('flip')
-        } else if(event.which == 38){
-            $('.control').teleport('move', 0, Number('-' + yspeed))
-        } else if(event.which == 40){
-            $('.control').teleport('move', 0, yspeed)
-        } else if(event.which == 116){
-            event.preventDefault()
-        } else if(event.which == 32){
-            if($('.control').hasClass('flip')){
-                $('.control').teleport('move', Number('-' + xspeed * 10), 0)
-            } else {
-                $('.control').teleport('move', xspeed * 10, 0)
+        if(window.start){
+            if($().overlap($('.control'), $('.enemy'))){
+                var overlap = window.overlap;
+                if(overlap.includes('santa')){
+                    $().intoFight('santa');
+                }
+            }
+            var xspeed = Number(window.xspeed);
+            var yspeed = Number(window.yspeed);
+            if(event.which == 39){
+                $('.control').teleport('move', xspeed, 0);
+                $('.control').removeClass('flip');
+            } else if(event.which == 37){
+                $('.control').teleport('move', Number('-' + xspeed), 0);
+                $('.control').addClass('flip');
+            } else if(event.which == 38){
+                $('.control').teleport('move', 0, Number('-' + yspeed));
+            } else if(event.which == 40){
+                $('.control').teleport('move', 0, yspeed);
+            } else if(event.which == 116){
+                event.preventDefault();
+            } else if(event.which == 32){
+                if($('.control').hasClass('flip')){
+                    $('.control').teleport('move', Number('-' + xspeed * 10), 0);
+                } else {
+                    $('.control').teleport('move', xspeed * 10, 0);
+                }
             }
         }
-    })
-})
+    });
+});
