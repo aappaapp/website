7e3477aa5202477d5fedb65a4b2d863bc035f0f5
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-11-04 18:24:16 +0800

    1

--- SolerFight/script.js
+++ SolerFight/script.js
@@ -14,8 +14,38 @@ function play_beta() {
     $('#warning').css('display', 'none');
     $('div:not(#warning)').css('display', 'inline-block');
 }
+function webapp() {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('/sw.js').then(function (registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(function (err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+    var CACHE_NAME = 'my-site-cache-v1';
+    var urlsToCache = [
+        '/',
+        '/styles/main.css',
+        '/script/main.js'
+    ];
+
+    self.addEventListener('install', function (event) {
+        // Perform install steps
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(function (cache) {
+                    console.log('Opened cache');
+                    return cache.addAll(urlsToCache);
+                })
+        );
+    });
+}
 $(document).ready(function () {
-    window.chosehero;
+    webapp();
     window.deviceType = getDeviceType();
     if (window.deviceType == 'deskatop') {
         $('div:not(#warning)').css('display', 'none');
