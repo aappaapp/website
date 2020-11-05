b086257328eb195a240f2b93704e545cb25118ed
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-11-05 21:40:09 +0800

    1

--- SolerFight/script.js
+++ SolerFight/script.js
@@ -16,36 +16,49 @@ function play_beta() {
 }
 function webapp() {
     if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function () {
-            navigator.serviceWorker.register('/sw.js').then(function (registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }).catch(function (err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
+        navigator.serviceWorker.register('/sw.js', { scope: '/' })
+            .then(function (registration) {
+                console.log('Service Worker Registered');
+            });
+
+        navigator.serviceWorker.ready.then(function (registration) {
+            console.log('Service Worker Ready');
+        });
+        caches.keys().then(function (cacheNames) {
+            cacheNames.forEach(function (cacheName) {
+                caches.delete(cacheName);
             });
         });
     }
-    var CACHE_NAME = 'my-site-cache-v1';
-    var urlsToCache = [
-        '/',
-        '/styles/main.css',
-        '/script/main.js'
-    ];
 
-    self.addEventListener('install', function (event) {
-        // Perform install steps
-        event.waitUntil(
-            caches.open(CACHE_NAME)
-                .then(function (cache) {
-                    console.log('Opened cache');
-                    return cache.addAll(urlsToCache);
-                })
-        );
-    });
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-59325548-2', 'auto');
+    ga('send', 'pageview');
+}
+$.fn.move = function (x, y) {
+    var x1 = Number(this.css('left').substr(0, this.css('left').length - 2));
+    var y1 = Number(this.css('top').substr(0, this.css('top').length - 2));
+    console.log(x1 + 'px');
+    console.log(x1 + x + 'px');
+    this.css('left', x1 + x + 'px');
+    this.css('top', y1 + y + 'px');
+}
+function generatescene() {
+    Math.floor(Math.random() * 101) + 1;
+    for (i = 1; i < 101; i++) {
+        $('#fightarea').append('<div class=\'block\'>block' + i + '</div>');
+    }
+    $('.block').wrapAll('<div class=\'scene\'></div>');
 }
 $(document).ready(function () {
     webapp();
+    generatescene();
     window.deviceType = getDeviceType();
     if (window.deviceType == 'deskatop') {
         $('div:not(#warning)').css('display', 'none');
@@ -78,5 +91,16 @@ $(document).ready(function () {
     $('#gamearea #startbtn').click(function () {
         $('#spriteselect').css('display', 'none');
         $('#fightarea').css('display', 'inline-block');
+    });
+    $(document).keydown(function () {
+        if (event.which == 39) {
+            $('#fightarea #sprite').move(10, 0);
+        } else if (event.which == 37) {
+            $('#fightarea #sprite').move(-10, 0);
+        } else if (event.which == 38) {
+            $('#fightarea #sprite').move(0, -10);
+        } else if (event.which == 40) {
+            $('#fightarea #sprite').move(0, 10);
+        }
     });
 });
