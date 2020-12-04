b66734b10d9d080831d9668819360cd88cd1273a
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-12-04 08:31:11 +0800

    1

---
+++ SolerFight/want to make.js
@@ -0,0 +1,15 @@
+wantmadenewgame = 'fit loser gh.';
+gh = 'ghost';
+//like undertale and deltarune
+charactor = {
+    'new': {
+        'snowman': {
+            'appearin': 'storymode',
+            'animation': {
+                'melt': 'leave and die'
+                //like undertalle flowey too.
+            }
+            //like undertalle flowey.
+        }
+    }
+}
