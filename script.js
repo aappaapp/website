eca0dcfaec5b8d0875bfdc8eff02317d9e2d91d4
adenpun <35649969+aappaapp@users.noreply.github.com>
2020-12-25 15:23:28 +0800

    1

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function home_page() {
-    window.top.location.href = 'index.html';
+    window.top.location.href = './index.html';
 }
 function faq_page() {
-    window.top.location.href = 'faq.html';
+    window.top.location.href = './faq.html';
 }
 function addon_page() {
-    window.top.location.href = 'mcaddon.html';
+    window.top.location.href = './mcaddon.html';
 }
 function page_choose() {
-    window.top.location.href = 'page_choose.html'
+    window.top.location.href = './page_choose.html'
 }
 function github_page() {
     window.top.location.href = 'https://github.com/adenpun/adenpun.github.io';
 }
 function tools_page() {
-    window.top.location.href = 'tools.html';
+    window.top.location.href = './tools.html';
 }
 function game_page(value) {
     if (value == 1) {
-        window.top.location.href = 'ADENTALE/index.html';
+        window.top.location.href = './ADENTALE';
     } else if (value == 2) {
-        window.top.location.href = 'humanvsmonster/index.html';
+        window.top.location.href = './humanvsmonster';
     }
 }
 console.log(window.location.host);
