a38482d3d04266281792de05c115f86d351a0be1
adenpun <35649969+aappaapp@users.noreply.github.com>
2021-01-20 08:59:53 +0800

    1

--- fakewncry/script.js
+++ fakewncry/script.js
@@ -30,6 +30,7 @@ firebase.database().ref('fakewncry/start').on('value', (snapshot) => {
                 var date2 = new Date;
                 var date3 = new Date(date - date2);
                 $('.payremaintime').text(date3.getHours() + ':' + date3.getMinutes() + ':' + date3.getSeconds());
+                console.log(date3);
                 if (date2 >= date && exeonce) {
                     alert('This is a prank!');
                     exeonce = false;
