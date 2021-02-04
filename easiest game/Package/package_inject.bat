5f38284cca08dd44219bece6fe72436e05d31ff6
adenpun <35649969+aappaapp@users.noreply.github.com>
2021-02-04 11:21:57 +0800

    1

--- easiest game/Package/package_inject.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo %~dp0inject.js
-for /r %%f in (*.txt) do (
-set name=%%~nf
-)
-set /p content=<"%name%.txt"
-title %name% Package Batch
-cd %HOMEPATH%/Downloads
-nativefier --name "%name%" --full-screen --inject "%~dp0inject.js" "%content%"
