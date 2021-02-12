94a11fae8e0921e4f944292ad803241c1d90dbb9
adenpun <35649969+aappaapp@users.noreply.github.com>
2021-02-12 21:33:08 +0800

    1

--- learnapp/script.js
+++ learnapp/script.js
@@ -1,6 +1,50 @@
+function getusername(uid, callback) {
+    firebase.database().ref('funvoc/user').once('value').then((snapshot) => {
+        const data = snapshot.val() || undefined;
+        var i;
+        var result;
+        for (i = 0; i < Object.keys(data).length; i++) {
+            if (data[Object.keys(data)[i]].uid == uid) {
+                result = Object.keys(data)[i];
+                window.getusernamev = result;
+                callback(result);
+            }
+        }
+    });
+}
 function afterloginorsignup() {
+    getusername($.cookie('uid'), function (data) {
+        firebase.database().ref('funvoc/user/' + data).on('value', (snapshot) => {
+            const data1 = snapshot.val() || undefined;
+            var i;
+            console.log(data1.folder);
+            $('.main.page .display').html('');
+        });
+    });
     $('.firsttimein.page').hide();
     $('.main.page').show();
+    $('.option.folder').click(function () {
+        $('.main.page').hide();
+        $('.makefolder.page').show();
+        $('.makefolder .create').click(function () {
+            getusername($.cookie('uid'), function (data) {
+                firebase.database().ref('funvoc/user/' + data + '/folder/' + $('.makefolder .name').val()).set({
+                    'sd': 'sd'
+                });
+            });
+            $('.main.page').show();
+            $('.makefolder.page').hide();
+            /*getusername($.cookie('uid'), function (data) {
+                firebase.database().ref('funvoc/user/' + data + '/folder').once('value').then((snapshot) => {
+                    var data1 = snapshot.val() || undefined;
+                    if (typeof data1 == 'undefined') {
+                        data1 = {};
+                    }
+                    firebase.database().ref('funvoc/user/' + data + '/folder/' + Object.keys(data1).length).set('sdsd');
+                });
+            });*/
+        });
+    });
 }
 $(function () {
     // Your web app's Firebase configuration
@@ -24,7 +68,11 @@ $(function () {
         fp.get().then(result => {
             // This is the visitor identifier:
             const visitorId = result.visitorId.substr(0, 4);
+            window.visitorId = visitorId;
             console.log('visitorId:', visitorId);
+            if ($.cookie('login') === 'true' && typeof $.cookie('uid') !== 'undefined') {
+                afterloginorsignup();
+            }
             $('.firsttimein .name').val('FunVoc_' + visitorId);
             $('.firsttimein .signup').click(function () {
                 var nameval = $('.firsttimein .name').val();
