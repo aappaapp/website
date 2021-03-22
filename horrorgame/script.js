7b89f3a0d2316aa4dca05bfb04ef4a1c2f0e9d5a
adenpun <35649969+aappaapp@users.noreply.github.com>
2021-03-22 08:08:56 +0800

    22/3/2021 8:08
    add soome new and useful item to horror game
    change some horrorgame code

--- horrorgame/script.js
+++ horrorgame/script.js
@@ -1,6 +1,23 @@
-function detectlanguage() { }
+function detectlanguage() {
+	window.language = navigator.language;
+	if (language.includes('zh')) language = 'zh';
+	else language = 'en';
+	$('select.language').val(language);
+}
+function languagechange() {
+	language = $('select.language').val();
+	dialogset();
+}
+function languagetextset() {
+	$('span.back').text(dialog['text.back']);
+}
+function dialogset() {
+	$.get('dialog-' + language + '.json', function (data) {
+		window.dialog = data;
+		languagetextset();
+	});
+}
 function changepage(selector, config) {
-	console.log(selector);
 	var returnval = '';
 	var originalpage = $('.page').filter(function () {
 		return $(this).css('background').length > 0;
@@ -14,13 +31,27 @@ function changepage(selector, config) {
 	return returnval;
 }
 function start() {
-	$.get('plot1.txt', function (data) {
-		plotdisplay(data, {
-			redirect: '.page.game'
-		});
+	plotdisplay(dialog['plot1'], {
+		redirect: '.page.game',
+		callback: function () {
+			speak({
+				text: dialog['main.whereteacher'],
+				callback: function () {
+					speak({
+						text: dialog['main.whereteacher2'],
+						callback: function () {
+							walk();
+							speak({
+								text: dialog['tutorial.wasd'],
+								finishtime: 10000
+							});
+						},
+					});
+				}
+			});
+		}
 	});
 	//changepage('.page.game');
-	charactercontrol();
 }
 function setting() {
 	changepage('.page.setting');
@@ -35,19 +66,27 @@ function move(x, y, ele) {
 		left: $(ele).position().left + x
 	});
 }
-function charactercontrol() {
-	$(document).on('keydown.character', function () {
-		if (window.keys[37]) {
-			move(-10, 0, $('.character.main'));
+function walk(speed) {
+	if (typeof speed == 'undefined') {
+		speed = 1;
+	}
+	if (typeof walkitv != 'undefined') {
+		clearInterval(walkitv);
+	}
+	window.walkitv = setInterval(function () {
+		$(document).on('keydown.character', function () {
+		});
+		if (window.keys[65]) {
+			move(-speed, 0, $('.character.main'));
 		}
-		if (window.keys[38]) {
-			move(0, -10, $('.character.main'));
+		if (window.keys[87]) {
+			move(0, -speed, $('.character.main'));
 		}
-		if (window.keys[39]) {
-			move(10, 0, $('.character.main'));
+		if (window.keys[68]) {
+			move(speed, 0, $('.character.main'));
 		}
-		if (window.keys[40]) {
-			move(0, 10, $('.character.main'));
+		if (window.keys[83]) {
+			move(0, speed, $('.character.main'));
 		}
 	});
 }
@@ -61,6 +100,7 @@ function plotdisplay(text, config) {
 		if (i == text.length + 1) {
 			setTimeout(function () {
 				clearInterval(textitv);
+				config.callback();
 				if (typeof config.redirect != 'undefined') {
 					changepage(config.redirect);
 				}
@@ -68,16 +108,42 @@ function plotdisplay(text, config) {
 		}
 	}, 100);
 }
+function speak(config) {
+	var i = 0;
+	var text = config.text || '';
+	var img = config.img || '';
+	var finishtime = config.finishtime || 500;
+	var callback = config.callback || function () { };
+	text = text.split('');
+	$('body').append('<div class=\'speakcontainer\'><img src=\'' + img + '\'></div>');
+	var textitv = setInterval(function () {
+		$('.speakcontainer').append(text[i]);
+		i++;
+		if (i == text.length + 1) {
+			setTimeout(function () {
+				clearInterval(textitv);
+				$('.speakcontainer').fadeOut(1000);
+				setTimeout(function () {
+					$('.speakcontainer').remove();
+					callback();
+				}, 1000);
+			}, finishtime);
+		}
+	}, 100);
+}
 $(function () {
 	//Init
 	detectlanguage();
+	dialogset();
 	window.keys = {};
 	//Start
 	$('.homestart').click(start);
 	//Setting
-	$('.setting').click(setting);
+	$('.select.setting').click(setting);
 	//Back
 	$('.select.back').click(back);
+	//Language Select
+	$('select.language').change(languagechange);
 	//Detect Keys
 	$(document).on('keydown.detectkeys', function () {
 		window.keys[event.which] = true;
