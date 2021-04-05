824a8c3b121332eef87f29d25900d7f9f465a2c3
adenpun <35649969+aappaapp@users.noreply.github.com>
2021-04-06 06:48:57 +0800

    6/4/2021 6:48
    code tools meta add scale
    add money

---
+++ money/script.js
@@ -0,0 +1,37 @@
+function main() {
+    var i = 0;
+    var startamount = Number($('.amount').text());
+    firebase.database().ref('money').once('value').then((snapshot) => {
+        var dv = snapshot.val().dv || 100;
+        var ps = snapshot.val().ps || '-';
+        var pv = snapshot.val().pv || 1;
+        var itv = setInterval(function () {
+            var amount = Number($('.amount').text());
+            $('.amount').text(eval('amount' + ps + pv));
+            var ifc;
+            if (ps == '+') {
+                ifc = startamount + dv - 1;
+            } else if (ps == '-') {
+                ifc = startamount - dv + 1
+            }
+            if (amount == ifc) {
+                clearInterval(itv);
+                if (pv != 1) {
+                    $('.amount').text(eval(startamount + ps + dv))
+                }
+                firebase.database().ref('money/start').set(false);
+            }
+        });
+    });
+}
+$(function () {
+    firebase.database().ref('money/start').on('value', (snapshot) => {
+        var data = snapshot.val() || false;
+        if (data) {
+            main();
+        }
+    });
+    $('.main').click(function () {
+        firebase.database().ref('money/start').set(true);
+    });
+});
