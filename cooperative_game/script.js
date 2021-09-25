ba77668451044fc4eae15261868b00eedd260e85
AdenPun <adenpun@gmail.com>
2021-09-25 23:46:19 +0800

    25/9/2021 23:45
    Add 50 Q&A
    
    And IDK

---
+++ cooperative_game/script.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDkSOCf6OlKlQK7dpJytnsZECWczfYApCo",
+    authDomain: "webdb200101.firebaseapp.com",
+    databaseURL: "https://webdb200101.firebaseio.com",
+    projectId: "webdb200101",
+    storageBucket: "webdb200101.appspot.com",
+    messagingSenderId: "485833164369",
+    appId: "1:485833164369:web:66144cf75de59218461a70",
+    measurementId: "G-DXYPZLMPD7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
