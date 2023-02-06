ec31e3a21710e5bd0323a5b6c91cc8398c248525
AdenPun <35649969+aappaapp@users.noreply.github.com>
2023-02-06 21:59:44 +0800

    Added google thing to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
             input: {
                 main: path.resolve(__dirname, "index.html"),
                 redirect: path.resolve(__dirname, "redirect.html"),
+                google: path.resolve(__dirname, "googled8bbdd240cc20c5d.html"),
             },
         },
     },
