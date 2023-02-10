2e58356bfd0cd6896332db5c3f764338f24400c0
AdenPun <35649969+aappaapp@users.noreply.github.com>
2023-02-10 20:52:54 +0800

    public

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
 import path from "node:path";
+import { defineConfig } from "vite";
+
 export default defineConfig({
+    appType: "mpa",
     build: {
         rollupOptions: {
             input: {
                 main: path.resolve(__dirname, "index.html"),
                 redirect: path.resolve(__dirname, "redirect.html"),
-                google: path.resolve(__dirname, "googled8bbdd240cc20c5d.html"),
             },
         },
     },
