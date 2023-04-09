265c63e51505e4c0dd2ac21515ca064610fb6460
AdenPun <35649969+aappaapp@users.noreply.github.com>
2023-04-10 02:27:00 +0800

    Updated stylse

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 const Article: Component<Props> = (props) => {
     return (
-        <article>
-            <a href={props.href}>
-                {props.title} -{" "}
+        <a href={props.href}>
+            <article>
+                {props.title + " "}-{" "}
                 {new Intl.DateTimeFormat(["en-GB"]).format(props.lastmod)}
-            </a>
-        </article>
+            </article>
+        </a>
     );
 };
 
