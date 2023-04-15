d4b80415b6aded2b3fd289ed4904e9bc1aad961c
AdenPun <35649969+aappaapp@users.noreply.github.com>
2023-04-16 03:47:58 +0800

    Update sw.js

--- public/tools/budget/sw.js
+++ public/tools/budget/sw.js
@@ -20,7 +20,7 @@ self.addEventListener("fetch", (e) => {
             const r = await caches.match(e.request);
             if (typeof r !== "undefined") return r;
             const response = await fetch(e.request);
-            const cache = await caches.open(cacheName);
+            const cache = await caches.open("budget");
             cache.put(e.request, response.clone());
             return response;
         })()
