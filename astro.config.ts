e3748837cb0b58f1ee842b970a60c2630105b9eb
Aden Pun <adenpun2000@gmail.com>
2023-09-13 19:12:03 +0800

    upgrade to Astro 3

--- astro.config.ts
+++ astro.config.ts
@@ -1,25 +1,20 @@
 import mdx from "@astrojs/mdx";
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 import redirects from "./src/data/redirect.json";
 
 const SITEMAP_EXCLUDE: string[] = [];
-const SITE = "https://adenpun.ml/";
+const SITE = "https://adenpun.net/";
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: netlify(),
-    experimental: {
-        redirects: true,
-    },
+    adapter: node({
+        mode: "standalone",
+    }),
     integrations: [
-        compress({
-            image: false,
-        }),
         mdx(),
         sitemap({
             filter: (page) => {
@@ -31,9 +26,7 @@ export default defineConfig({
         }),
         svelte(),
         tailwind({
-            config: {
-                applyBaseStyles: false,
-            },
+            applyBaseStyles: false,
         }),
     ],
     output: "hybrid",
--- src/content/articles/my-first-blog.md
+++ src/content/articles/my-first-blog.md
@@ -1,5 +1,5 @@
 ---
-lastmod: 2022-04-11T17:00:00.000Z
+lastmod: 2023-09-13T19:00:00.000Z
 publish: 2022-03-19T04:00:00.000Z
 title: My First Blog!
 ---
@@ -12,7 +12,7 @@ Hello, my freinds! If you are watching this, how??? I didn't do any SEO, or any 
 
 ## So, who am I?
 
-I am a young human who likes programming and video games. I am not a very smart guy. I don't even know how to write a blog. I use a very cheap (~HK$5000) HP laptop to program and play games(Minecraft, Undertale, Celeste...).
+I am a young human who likes programming and video games. I am not a very smart guy. I don't even know how to write a blog. I use a very cheap (~HK$5000) HP laptop to program and play games(Minecraft, Undertale, Celeste, Hollow Knight...).
 
 ## Conclusion
 
